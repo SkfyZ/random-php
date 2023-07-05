09e5ba29a08c605796d7eda47379ab85968331e8
SkfyZ <SkfyZ@outlook.com>
2023-07-05 22:29:58 +0800

    Update index.js

--- search/index.js
+++ search/index.js
@@ -1,5 +1,5 @@
 const querysearch = window.location.search;
-const url = 'https://api.waifu.ey.ax/search${querysearch}';
+const url = `https://api.waifu.ey.ax/search${querysearch}`;
 var cdnurl = 'https://cdn.waifu.ey.ax/' ;
 function get() {       
     return new Promise(function (resolve, reject) {      
