2d610db6c6bfcc1645b218fb76706bb7ecb42baa
SkfyZ <SkfyZ@outlook.com>
2023-07-09 20:29:06 +0800

    Update im.php

--- www/im.php
+++ www/im.php
@@ -1,23 +1,28 @@
 <?php
     $cdnurl = "https://waifu-im.cdn.ey.ax/";
-	if (isset($_SERVER['QUERY_STRING'])) {
-		$query = $_SERVER['QUERY_STRING'];
-  		parse_str($query, $params);
-
-  		$allowedParams = [
-  	      'included_tags', 'excluded_tags', 'included_files', 'excluded_files',
-  	      'is_nsfw', 'gif', 'order_by', 'orientation', 'many', 'full',
- 	       'width', 'height', 'byte_size'
- 		];
 	
- 		foreach ($params as $name => $value) {
+    if (isset($_SERVER['QUERY_STRING'])) {
+        $query = $_SERVER['QUERY_STRING'];
+        parse_str($query, $params);
+
+        $allowedParams = [
+            'included_tags', 'excluded_tags', 'included_files', 'excluded_files',
+            'is_nsfw', 'gif', 'order_by', 'orientation', 'many', 'full',
+            'width', 'height', 'byte_size'
+        ];
+	
+        foreach ($params as $name => $value) {
         if (!in_array($name, $allowedParams)) {
             unset($params[$name]);
         }
     }
 
     $query = http_build_query($params);
-}
+	}
+    else {
+        $query = "included_tags=ass&gif=false";
+    }
+	
 	
     $json = file_get_contents('https://waifu-im.api.ey.ax/search?'. $query);
     $data = json_decode($json);
