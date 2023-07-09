0648a594312cb23605327c042ad4e999f00f1cc5
SkfyZ <SkfyZ@outlook.com>
2023-07-09 12:40:54 +0800

    test

--- search/index.js
+++ search/index.js
@@ -1,15 +1,16 @@
 const querysearch = window.location.search;
-const url = `https://api.waifu.ey.ax/search${querysearch}`;
-var cdnurl = 'https://cdn.waifu.ey.ax/' ;
-function get() {       
-    return new Promise(function (resolve, reject) {      
+const url = `https://waifu.api.ey.ax/search${querysearch}`;
+var cdnurl = 'https://waifu.cdn.ey.ax/';
+
+function get() {
+    return new Promise(function (resolve, reject) {
         var req = new XMLHttpRequest();
         req.open('GET', url, true);
-		req.responseType = 'json';
+        req.responseType = 'json';
         req.onload = function () {
             if (req.status == 200) {
                 resolve(req.response.images[0].url);
-				imageUrl = cdnurl + req.response.images[0].image_id + req.response.images[0].extension
+                imageUrl = cdnurl + req.response.images[0].image_id + req.response.images[0].extension
             } else {
                 reject(Error(req.statusText));
             }
@@ -22,7 +23,7 @@ function get() {
 }
 
 get().then(function (response) {
-window.location.replace(imageUrl); 
+    window.location.replace(imageUrl);
 }, function (error) {
     console.error("Failed!", error);
-}); 
+});
